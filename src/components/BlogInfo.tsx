7f874edf6d6a2250175df4ee491553563ff418ba
christianmacasinagrodriguez <140526893+christianmacasinagrodriguez@users.noreply.github.com>
2023-10-09 06:58:20 +0800

    Change the dispaly from grid to flex

--- src/components/BlogInfo.tsx
+++ src/components/BlogInfo.tsx
@@ -5,18 +5,18 @@ import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
 
 const BlogInfo = () => {
   return (
-    <div className="grid grid-cols-4 md:grid-cols-7 lg:grid-cols-7 xl:grid-cols-7 divide-x pt-6 pb-4 gap-5 text-base text-[#c5c5ca]">
-      <div className="flex flex-row gap-1">
+    <div className="flex flex-row justify-evenly md:justify-start lg:justify-start xl:justify-start divide-x pt-6 pb-4 gap-5 text-base text-[#c5c5ca]">
+      <div className="flex flex-row gap-1 pr-4">
         <ScheduleIcon />
 
         <span>15 Min</span>
       </div>
-      <div className="flex flex-row justify-center gap-1">
+      <div className="flex flex-row justify-center gap-1 px-4">
         <VisibilityIcon />
 
         <span>55</span>
       </div>
-      <div className="flex flex-row justify-start col-span-2 gap-1 pl-4">
+      <div className="flex flex-row justify-start gap-1 px-4">
         <PersonOutlineIcon />
 
         <span>By Admin</span>
