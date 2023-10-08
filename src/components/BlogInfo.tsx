6a677298154a9da2c19c242a969265c0e004501e
christianmacasinagrodriguez <140526893+christianmacasinagrodriguez@users.noreply.github.com>
2023-10-09 06:46:43 +0800

    Fix the BlogInfo display

--- src/components/BlogInfo.tsx
+++ src/components/BlogInfo.tsx
@@ -5,7 +5,7 @@ import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
 
 const BlogInfo = () => {
   return (
-    <div className="grid grid-cols-3 md:grid-cols-7 lg:grid-cols-7 xl:grid-cols-7 divide-x pt-6 pb-4 gap-5 text-base text-[#c5c5ca]">
+    <div className="grid grid-cols-4 md:grid-cols-7 lg:grid-cols-7 xl:grid-cols-7 divide-x pt-6 pb-4 gap-5 text-base text-[#c5c5ca]">
       <div className="flex flex-row gap-1">
         <ScheduleIcon />
 
@@ -16,7 +16,7 @@ const BlogInfo = () => {
 
         <span>55</span>
       </div>
-      <div className="flex flex-row justify-start gap-1 pl-4">
+      <div className="flex flex-row justify-start col-span-2 gap-1 pl-4">
         <PersonOutlineIcon />
 
         <span>By Admin</span>
