0b948af6ce75d22e166e43aeb1a4f8e579791266
christianmacasinagrodriguez <140526893+christianmacasinagrodriguez@users.noreply.github.com>
2023-10-09 00:13:36 +0800

    Fix

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -21,6 +21,22 @@ const BlogsPage = () => {
         imgSrc="https://steamuserimages-a.akamaihd.net/ugc/2094792567187206166/6A15DE0D9D7E3EE50E4A93EBF17F0C269C46E4C6/?imw=1920&&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false"
         blogTitle="Exploring Different Genres In Game Design And Development"
       />
+      <BlogCard
+        imgSrc="https://steamuserimages-a.akamaihd.net/ugc/2109429266524096918/02032A5360DA8D57374F407922254E1932B4D01A/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false"
+        blogTitle="The Role Of Music And Sound Effects In Game Design"
+      />
+      <BlogCard
+        imgSrc="https://steamuserimages-a.akamaihd.net/ugc/2122941552022954657/F0C2C749BBD2C5A9D270BB1BA8FF0983EB7765C2/?imw=1920&&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false"
+        blogTitle="The Importance Of User Testing In Game Development"
+      />
+      <BlogCard
+        imgSrc="https://steamuserimages-a.akamaihd.net/ugc/2068898300047349186/3732B1308C9DF46D7270AA938ADC34D8E724A537/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false"
+        blogTitle="Creating Memorable Characters In Video Games"
+      />
+      <BlogCard
+        imgSrc="https://steamuserimages-a.akamaihd.net/ugc/2094792567187206166/6A15DE0D9D7E3EE50E4A93EBF17F0C269C46E4C6/?imw=1920&&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false"
+        blogTitle="Exploring Different Genres In Game Design And Development"
+      />
 
       <Pagination />
     </section>
